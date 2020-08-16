bcb7beaa1a5adbd02834c08b89ac2671052887bf
zcong1993 <zhangcong1992@gmail.com>
2020-08-16 17:10:11 +0800

    feat: support push

--- entrypoint.js
+++ entrypoint.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const { GITHUB_TOKEN, GITHUB_EVENT_NAME } = require("./src/constants");
+const { GITHUB_TOKEN, GITHUB_EVENT_NAME, GITHUB_REF } = require("./src/constants");
 
 const githubEvent = require("./src/github-event");
 const run = require("./src/index.js");
+const { runPush } = require('./src/push')
 
 if (!GITHUB_TOKEN) {
   console.log("::error:: You must enable the GITHUB_TOKEN secret");
@@ -11,24 +12,32 @@ if (!GITHUB_TOKEN) {
 }
 
 const main = async () => {
-  // Bail out if the event that executed the action wasn’t a pull_request
-  if (GITHUB_EVENT_NAME !== "pull_request") {
-    console.log("::error:: This action only runs for pushes to PRs");
-    process.exit(78);
-  }
-
-  // Bail out if the pull_request event wasn't synchronize or opened
   const event = await githubEvent();
-  if (event.action !== "synchronize" && event.action !== "opened") {
-    console.log(
-      "::error:: Check run has action",
-      event.action,
-      ". Wants: synchronize or opened"
-    );
-    process.exit(78);
+  if (GITHUB_EVENT_NAME === 'pull_request') {
+    if (event.action !== "synchronize" && event.action !== "opened") {
+      console.log(
+        "::error:: Check run has action",
+        event.action,
+        ". Wants: synchronize or opened"
+      );
+      process.exit(78);
+    }
+
+    await run();
+    return
   }
 
-  await run();
+  if (GITHUB_EVENT_NAME === 'push') {
+    if (!GITHUB_REF.match(/^refs\/heads\//)) {
+      console.log(`ignore none branch ref ${GITHUB_REF}`)
+      return
+    }
+    await runPush()
+    return
+  }
+
+  console.log("::error:: Invalid events");
+  process.exit(78);
 };
 
 main();
---
+++ src/push.js
@@ -0,0 +1,146 @@
+const fs = require("fs").promises;
+const generateMarkdownReport = require("./github-markdown");
+const processImages = require("./image-processing");
+const github = require("./github-api");
+const githubEvent = require("./github-event");
+
+const { GITHUB_REF } = require('./constants')
+
+const logPrefix = `[PUSH]`
+
+const convertToTreeBlobs = async ({ owner, repo, images }) => {
+  console.log("\t * ", "Converting images to blobs…");
+  const imageBlobs = [];
+
+  for await (const image of images) {
+    const encodedImage = await fs.readFile(image.path, { encoding: "base64" });
+
+    const blob = await api.git.createBlob({
+      owner,
+      repo,
+      content: encodedImage,
+      encoding: "base64"
+    });
+
+    // We use image.name rather than image.path because it is the path inside the repo
+    // rather than the path on disk (which is static/images/image.jpg rather than /github/workpace/static/images/image.jpg)
+    imageBlobs.push({
+      path: image.name,
+      type: "blob",
+      mode: "100644",
+      sha: blob.data.sha
+    });
+  }
+
+  return imageBlobs;
+};
+
+const toUpdateRef = ref => ref.replace(/^refs\//, '')
+
+const getBranch = ref => {
+  const tmpArr = ref.split('/')
+  return tmpArr[tmpArr.length - 1]
+}
+
+const getLastSha = async (owner, repo, ref) => {
+  const res = await github.git.getRef({
+    owner,
+    repo,
+    ref
+  })
+
+  return res.data.object.sha
+}
+
+const createPr = async (optimisedImages, markdown) => {
+  const event = await githubEvent();
+  const owner = event.repository.owner.login;
+  const repo = event.repository.name;
+  const baseRef = GITHUB_REF
+  const destRef = GITHUB_REF + '-image-bot'
+
+  const ls = await getLastSha(owner, repo, toUpdateRef(baseRef))
+  // create branch
+  const { data: ref } = await github.git.createRef({
+    owner,
+    repo,
+    ref: destRef,
+    sha: ls
+  })
+  console.log('create new branch')
+  // create blobs
+  const treeBlobs = await convertToTreeBlobs({
+    owner,
+    repo,
+    images: optimisedImages
+  });
+
+  console.log('create blobs')
+
+  // create commit tree
+  const { data: newTree } = await github.git.createTree({
+    owner,
+    repo,
+    tree: treeBlobs,
+    base_tree: ref.object.sha
+  })
+  console.log('new tree')
+
+  // create commit
+  const { data: commit } = await github.git.createCommit({
+    owner,
+    repo,
+    message: 'Optimised images',
+    tree: newTree.sha,
+    parents: [ref.object.sha]
+  })
+  console.log('create commit')
+
+  // update ref
+  const { data: ref2 } = await github.git.updateRef({
+    owner,
+    repo,
+    ref: toUpdateRef(destRef),
+    sha: commit.sha
+  })
+
+  console.log('update ref')
+
+   // create pr
+   await github.pulls.create({
+    owner,
+    repo,
+    base: getBranch(baseRef),
+    head: getBranch(destRef),
+    body: markdown,
+    title: '[Image Bot] Optimised images'
+  })
+}
+
+const runPush = async () => {
+  console.log(logPrefix, " ->> Locating images…");
+
+  const results = await processImages();
+
+  const optimisedImages = results.images.filter(
+    img => img.compressionWasSignificant
+  );
+
+  // If nothing was optimised, bail out.
+  if (!optimisedImages.length) {
+    console.log("::warning:: Nothing left to optimise. Stopping…");
+    return;
+  }
+
+  console.log("->> Generating markdown…");
+  const markdown = generateMarkdownReport(results);
+
+  console.log("->> Committing files…");
+  await createPr(optimisedImages, markdown);
+
+  return results;
+}
+
+module.exports = {
+  runPush
+}
